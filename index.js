8a37bb65cb6a75f6e2da7a65c213db65664a9055
ybmarbaniang-spec <ybmarbaniang@gmail.com>
2026-04-19 03:58:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!'
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  try {
+    console.log('Registering slash commands...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        client.user.id,
+        'YOUR_GUILD_ID'
+      ),
+      { body: commands }
+    );
+
+    console.log('Slash commands registered!');
+  } catch (err) {
+    console.error(err);
+  }
+});
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
