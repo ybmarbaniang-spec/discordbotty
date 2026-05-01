7eb1577dcbd1422d625b1ac7893a9115a3d6ffa8
ybmarbaniang-spec <ybmarbaniang@gmail.com>
2026-05-01 18:49:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ const commands = [
     { name: 'user', type: 6, required: true, description: 'User' },
     { name: 'role', type: 8, required: true, description: 'Role' }
   ]}
-];,
+]},
 
   { name: 'announce', description: 'Send an announcement to a channel', options: [
   { name: 'channel', type: 7, required: true, description: 'Channel to announce in' },
